62640b249a27d080269cf8cd1ee6be375e31f99e
miqla <miqdad.shaldin@gmail.com>
2023-09-28 21:00:21 +0700

    memperbaiki rumus yg ada di angkot 1

--- angkot.js
+++ angkot.js
@@ -1,7 +1,16 @@
 //  ----- Angkot 1 ------
 
-var angkot = 1;
-while (angkot <= 10) {
-  console.log("Angkot No. " + angkot + " beroperasi dengan baik");
-  angkot++;
+// var angkot = 1;
+// while (angkot <= 10) {
+//   console.log("Angkot No. " + angkot + " beroperasi dengan baik");
+//   angkot++;
+// }
+
+//  ----- Angkot 1: Benerin rumus ------
+
+var jmlAngkot = 10;
+var noAngkot = 1;
+while (noAngkot <= jmlAngkot) {
+  console.log("Angkot No. " + noAngkot + " beroperasi dengan baik");
+  noAngkot++;
 }
