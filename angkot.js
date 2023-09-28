42b23f7b626a05b3a4d584e1d7e75a032a50aeac
miqla <miqdad.shaldin@gmail.com>
2023-09-28 23:44:30 +0700

    Menambahkan Angkot 3, Latihan else if, dan menambahkan angkot 4

--- angkot.js
+++ angkot.js
@@ -19,17 +19,52 @@
 // }
 
 // ----- Angkot 2 -----------
-var jmlAngkot = 10;
-var angkotBeroperasi = 8;
-var noAngkot = 1;
-while (noAngkot <= angkotBeroperasi) {
-  console.log("Angkot No. " + noAngkot + " Beroperasi dengan baik");
-  noAngkot++;
-}
-for (noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
-  console.log("Angkot No. " + noAngkot + " Sedang tidak beroperasi");
-}
+// var jmlAngkot = 10;
+// var angkotBeroperasi = 8;
+// var noAngkot = 1;
+// while (noAngkot <= angkotBeroperasi) {
+//   console.log("Angkot No. " + noAngkot + " Beroperasi dengan baik");
+//   noAngkot++;
+// }
+// for (noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
+//   console.log("Angkot No. " + noAngkot + " Sedang tidak beroperasi");
+// }
 // ----- Cara kedua untuk for
 // for (noAngkot = angkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++) {
 //   console.log("Angkot No. " + noAngkot + " Sedang tidak beroperasi");
 // }
+
+//  ----- Angkot 3 -------
+// var jmlAngkot = 10;
+// var angkotBeroperasi = 7;
+
+// for (var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++) {
+//   if (noAngkot <= angkotBeroperasi) {
+//     console.log("Angkot No. " + noAngkot + " Beroperasi dengan baik");
+//   } else {
+//     console.log("Angkot No. " + noAngkot + " Sedang tidak beroperasi");
+//   }
+// }
+
+//  ---- Latihan else if -----
+// var angka = prompt("Masukkan Angka");
+// if (angka % 2 === 0) {
+//   alert(angka + " Adalah bilangan GENAP");
+// } else if (angka % 2 === 1) {
+//   alert(angka + " Adalah bilangan GANJIL");
+// } else {
+//   alert("Yang Anda masukkan bukan angka!!");
+// }
+
+// ------ Angkot 4 ------
+var jmlAngkot = 10;
+var angkotBeroperasi = 6;
+for (var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++) {
+  if (noAngkot <= angkotBeroperasi) {
+    console.log("Angkot No. " + noAngkot + " Beroperasi dengan baik");
+  } else if (noAngkot === 8) {
+    console.log("Angkot No. " + noAngkot + " Sedang lembur");
+  } else {
+    console.log("Angkot No. " + noAngkot + " Sedang tidak beroperasi");
+  }
+}
