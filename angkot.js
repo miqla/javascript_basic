9b5c2c0e92ef8f6c658c849067376febca2470ba
miqla <miqdad.shaldin@gmail.com>
2023-09-28 22:57:14 +0700

    menambahkan contoh penggunaan for, dan latihan angkot 2

--- angkot.js
+++ angkot.js
@@ -1,5 +1,4 @@
 //  ----- Angkot 1 ------
-
 // var angkot = 1;
 // while (angkot <= 10) {
 //   console.log("Angkot No. " + angkot + " beroperasi dengan baik");
@@ -7,10 +6,30 @@
 // }
 
 //  ----- Angkot 1: Benerin rumus ------
+// var jmlAngkot = 10;
+// var noAngkot = 1;
+// while (noAngkot <= jmlAngkot) {
+//   console.log("Angkot No. " + noAngkot + " beroperasi dengan baik");
+//   noAngkot++;
+// }
 
+//  ----- Contoh pake for -------
+// for (var angkot = 1; angkot <= 11; angkot++) {
+//   console.log("Angkot No. " + angkot + " Beroperasi dengan baik");
+// }
+
+// ----- Angkot 2 -----------
 var jmlAngkot = 10;
+var angkotBeroperasi = 8;
 var noAngkot = 1;
-while (noAngkot <= jmlAngkot) {
-  console.log("Angkot No. " + noAngkot + " beroperasi dengan baik");
+while (noAngkot <= angkotBeroperasi) {
+  console.log("Angkot No. " + noAngkot + " Beroperasi dengan baik");
   noAngkot++;
 }
+for (noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
+  console.log("Angkot No. " + noAngkot + " Sedang tidak beroperasi");
+}
+// ----- Cara kedua untuk for
+// for (noAngkot = angkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++) {
+//   console.log("Angkot No. " + noAngkot + " Sedang tidak beroperasi");
+// }
