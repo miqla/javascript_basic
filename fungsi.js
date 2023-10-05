666f59f9a5e6207586a5df2a3fe3432295edd247
miqla <miqdad.shaldin@gmail.com>
2023-10-05 20:24:09 +0700

    menambahkan cara menggunakan pseudo variabel yaitu arguments

--- fungsi.js
+++ fungsi.js
@@ -6,3 +6,14 @@ function volumeDuaKubus(a, b) {
 }
 console.log(volumeDuaKubus(8, 3));
 console.log(volumeDuaKubus(4, 9));
+
+// Arguments = Array yg menyimpan argument yg dikirimkan sebagai input parameter
+function tambah() {
+  var hasil = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    hasil += arguments[i];
+  }
+  return hasil;
+}
+coba = tambah(1, 5, 8, 7, 6, 4);
+console.log(coba);
