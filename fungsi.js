a38e0faac1d2bc28766e011a67fb6aa2e305a414
miqla <miqdad.shaldin@gmail.com>
2023-10-05 19:34:53 +0700

    menambahkan file fungsi.js yg berisi function jumlah volume 2 kubus, lalu merubah isi index.html utk memanggil fungsi.js

---
+++ fungsi.js
@@ -0,0 +1,8 @@
+function volumeDuaKubus(a, b) {
+  var volA = a * a * a;
+  var volB = b * b * b;
+  var hasil = volA + volB;
+  return hasil;
+}
+console.log(volumeDuaKubus(8, 3));
+console.log(volumeDuaKubus(4, 9));
