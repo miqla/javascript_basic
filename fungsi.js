4182df0fa55fd44a2ae8dbb0738360eac8fdb73b
miqla <miqdad.shaldin@gmail.com>
2023-10-05 22:28:29 +0700

    menambahkan fungsi rekursif untuk menampilkan angka dan faktorial

--- fungsi.js
+++ fungsi.js
@@ -23,3 +23,19 @@ function jumlahVolumeDuaKubus(a, b) {
   return a * a * a + b * b * b;
 }
 console.log(jumlahVolumeDuaKubus(8, 3));
+
+// Fungsi rekursif
+function tampilAngka(n) {
+  if (n === 0) {
+    return;
+  }
+  console.log(n);
+  tampilAngka(n - 1);
+}
+tampilAngka(5);
+
+function faktorial(n) {
+  if (n === 0) return 1;
+  return n * faktorial(n - 1);
+}
+console.log(faktorial(5));
