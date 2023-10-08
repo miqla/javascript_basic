72c0c11f710f0b6cbb4c899e0d218bccb6bca958
miqla <miqdad.shaldin@gmail.com>
2023-10-08 20:54:09 +0700

    membuat file object.js, latihan membuat object literal, mengubah index.html memanggil object.js

---
+++ object.js
@@ -0,0 +1,11 @@
+// Membuat object (literal)
+var mahasiswa = {
+  nama: "Mila",
+  umur: 25,
+  lulus: true,
+  alamat: {
+    jalan: "Jalan pegangsaan timur no 2",
+    kota: "bandung",
+    provinsi: "Jawa Barat",
+  },
+};
