9ff2797b8cba2fac4ea60be06578eaf44a710c27
miqla <miqdad.shaldin@gmail.com>
2023-10-08 21:25:16 +0700

    menambahkan cara membuat object dengan function declaratin dan constructor

--- object.js
+++ object.js
@@ -1,5 +1,5 @@
 // Membuat object (literal)
-var mahasiswa = {
+var mahasiswaO = {
   nama: "Mila",
   umur: 25,
   lulus: true,
@@ -9,3 +9,26 @@ var mahasiswa = {
     provinsi: "Jawa Barat",
   },
 };
+
+// Object dengan function declaration
+function membuatObjectMahasiswa(nama, umur, jurusan) {
+  // Deklarasikan dulu
+  var mhs = {};
+  mhs.nama = nama;
+  mhs.umur = umur;
+  mhs.jurusan = jurusan;
+  return mhs; //kembalikan nilai
+}
+// menambahkan data dengan memanggil funtion
+mhs2 = membuatObjectMahasiswa("agus", 26, "Teknik Kimia");
+
+// Constructor
+function Mahasiswa(nama, umur, jurusan) {
+  // var this = {};
+  this.nama = nama;
+  this.umur = umur;
+  this.jurusan = jurusan;
+  // return this;
+}
+// memanggil constructor harus ada new
+mhs3 = new Mahasiswa("erik", 22, "Teknik Mesin");
